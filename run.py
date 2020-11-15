d4b65628b629ecbcaec22f8f6f0d7a11782d586b
Kamui-Jiao <48661578+Kamui-Jiao@users.noreply.github.com>
2020-11-15 17:21:38 +0800

    Update run.py

--- run.py
+++ run.py
@@ -8,62 +8,55 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-def main(path_m, path_s):
+import build_features
+from model import mse
+
+def main(target):
     '''
     Runs the main project pipeline logic.
     
     '''
+    if 'feature' in targets:
+        with open('feature.json') as fh:
+            data_cfg = json.load(fh)
 
-    def generate_feature(path, ware_name, is_malware):
-        sml = ''
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in files:
-                if name.endswith('.smali'):
-                    f = open(os.path.join(root, name))
-                    sml += f.read() + '\n'
-                    f.close()
-        df = pd.DataFrame(re.findall(r'invoke-(\w{5,9})\s.+}, (.*);->', sml))
-        nums = df[0].value_counts()
-        def get_num(name):
-            try:
-                return nums[name]
-            except:
-                return 0
-        res = [ware_name, is_malware, df.shape[0], len(df[1].unique()), len(re.findall(r'\.method', sml)), get_num('direct'), get_num('static'), get_num('virtual'), get_num('interface'), get_num('super')]
+        # make the data target
+        features_mal = parse_all(data_cfg['path_mal'], 1)
+        features_saf = parse_all(data_cfg['path_saf'], 0)
+        features = features_mal + features_saf
+        X = [i[2:] for i in features]
+        y = [i[1] for i in features]
+        
+    if 'analysis' in targets:
+        with open('eda.json') as fh:
+            data_cfg = json.load(fh)
 
-        return res
-    
-    def mse(X, y):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle = True)
-        reg = LinearRegression().fit(X_train, y_train)
-        y_predict_train = reg.predict(X_train)
+        # make the data target
+        plots = eval(data_cfg['plot'])
+        df = pd.DataFrame(features, columns=['app_name', 'is_malware', 'num_smali','num_api', 'num_unique_api','num_block',
+                                                 'num_direct', 'num_static', 'num_virtual', 'num_interface', 'num_super'])
+        if 'boxplot' in plots:
+            df.boxplot(column=['num_api', 'num_static', 'num_unique_api'])
+        
+        if 'describe' in plots:
+            df.describe()
+            
+        if 'api_count' in plots:
+            A = matrix_A(features, data_cfg['total_col'])
+            A.describe()
+            
+    if 'model' in targets:
+        with open('model.json') as fh:
+            data_cfg = json.load(fh)
+
+        # make the data target
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle = True)        
+        reg = LogisticRegression(fit_intercept=True, C=data_cfg['C'], max_iter=data_cfg['max_iter']).fit(X_train, y_train)        
+        y_predict_train = reg.predict(X_train)        
         y_predict_test = reg.predict(X_test)
-        print('train MSE = {}, test MSE = {}'.format(mean_squared_error(y_train, y_predict_train), 
-                                                     mean_squared_error(y_test, y_predict_test)))
-        
-        return mean_squared_error(y_test, y_predict_test)
-        
-    def run(path, is_mal):
-        res = []
-        wares = [i for i in os.listdir(path) if '.' not in i]
-        for d in wares:
-            d_path = path + '/' + i
-            res.append(generate_feature(d_path, d, is_mal))
-        return res
-      
-    # Malware
-    path_mal = '/teams/DSC180A_FA20_A00/a04malware/malware' # Path
-    features_mal = run(path_mal, 1)
+        mse(y_train, y_predict_train, y_test, y_predict_test)
     
-    # Safeware
-    path_saf = '/teams/DSC180A_FA20_A00/a04malware/test-apps/benign' # Path
-    features_saf = run(path_saf, 0)
-    
-    features = features_mal + features_saf
-    X = [i[2:] for i in features]
-    y = [i[1] for i in features]
-    
-    return mse(X, y)
+    return reg
 
 
 if __name__ == '__main__':
