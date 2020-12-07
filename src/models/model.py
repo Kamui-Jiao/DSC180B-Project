185467560c3332b7197cbe20e167327b5fb9785f
Kamui-Jiao <48661578+Kamui-Jiao@users.noreply.github.com>
2020-12-07 13:03:57 +0800

    Update model.py

--- src/models/model.py
+++ src/models/model.py
@@ -8,7 +8,7 @@ def mse(X_train, X_test, y_train, y_test, reg):
                                                  mean_squared_error(y_test, y_predict_test)))
     return mean_squared_error(y_test, y_predict_test)
 
-def build(X, y):
+def build(X, y, C, max_it):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle = True)        
-    reg = LogisticRegression(fit_intercept=True, C=data_cfg['C'], max_iter=data_cfg['max_iter']).fit(X_train, y_train)
+    reg = LogisticRegression(fit_intercept=True, C=C, max_iter=max_it).fit(X_train, y_train)
     return X_train, X_test, y_train, y_test, reg
