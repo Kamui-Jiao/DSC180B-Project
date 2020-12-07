9f44c9895888a6eccaf7f30d693becd48da0dd8a
Kamui-Jiao <48661578+Kamui-Jiao@users.noreply.github.com>
2020-12-07 12:30:10 +0800

    Update eda.py

--- src/analysis/eda.py
+++ src/analysis/eda.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 
-def 
+def eda(df, plots):
+    if 'boxplot' in plots:
+        df.boxplot(column=['num_api', 'num_static', 'num_unique_api'])
+        
+    if 'describe' in plots:
+        df.describe()
+        
+    if 'api_count' in plots:
+        A = matrix_A(features, data_cfg['total_col'])
+        A.describe()
