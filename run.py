55e547b578d7604a637a60e36856fd2c4b290228
Kamui-Jiao <j7jiao@ucsd.edu>
2021-02-08 13:39:31 +0800

    Commit all

--- run.py
+++ run.py
@@ -4,9 +4,6 @@ import re
 import pandas as pd
 import os
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 import src.features.feature
 import src.models.model
@@ -24,11 +21,8 @@ def main(target):
             data_cfg = json.load(fh)
 
         # make the data target
-        features_mal = parse_all(data_cfg['path_mal'], 1, data_cfg['pattern'])
-        features_saf = parse_all(data_cfg['path_saf'], 0, data_cfg['pattern'])
-        features = features_mal + features_saf
-        df = pd.DataFrame(features, columns=data_cfg['column_names'])
-        X, y = generate_Xy(features)
+        features_mal = parse_mal(data_cfg['path_mal'])
+        features_saf = parse_common(data_cfg['path_saf'])
 
     if 'analysis' in targets:
         with open('config/eda.json') as fh:
@@ -43,23 +37,12 @@ def main(target):
             data_cfg_model = json.load(fh)
 
         # make the data target
-        #X_train, X_test, y_train, y_test = ttsplit(X, y)
-        #reg = build(X_train, y_train, data_cfg_model['C'], data_cfg_model['max_iter'])
-        #pred_train = reg.predict(X_train)
-        #pred_test = reg.predict(X_test)
-        #mse_1 = mse(y_train, y_test, pred_train, pred_test)
 
     if 'test' in targets:
         # make the data target
-        feature_test = generate_feature('test', 'test', 1, data_cfg['pattern'])
-        X_t, _ = generate_Xy(feature_test)
-        df = pd.DataFrame(feature_test, columns=data_cfg['column_names'])
-        # output type I
-        df.describe().to_csv('describe.csv')
 
-        # output type II
-        #pred = reg.predict(X_t)
-        return df
+        # output type I
+        return
 
     return 'Finished'
 
--- src/features/feature.py
+++ src/features/feature.py
@@ -4,28 +4,48 @@ import pandas as pd
 import os
 import numpy as np
 
-# Parse specific ware and generate features
-def generate_feature(path, ware_type, is_malware):
-    sml = ''
-    i = 0
+# Transform smali file to call-anlysis table
+def process_smali(sml):
+    methods = pd.DataFrame(re.findall(r'method.* (\w+)[(].+[)].+;([\d\D]*?)\.end method', sml))
+    if len(methods) == 0:
+        return -1
+
+    def process_method(x):
+        res = pd.DataFrame(re.findall(r'invoke-(\w{5,9})\s.+}, (.*);->(.+)[(]', x[1]))
+        if len(res) == 0:
+            return
+        res.columns = ['invoke_type', 'package_long', 'call']
+        res['method'] = x[0]
+        res['package'] = res.package_long.apply(lambda x:x[1:x.find('/')])
+        res['type'] = res.package.apply(lambda x:x if x in ['android', 'androidx', 'google', 'java', 'javax', 'kotlin'] else 'self')
+        return res
+
+    dfs = methods.apply(process_method, axis=1)
+    try:
+        return pd.concat(dfs.tolist())
+    except:
+        return -1
+
+
+# Generate Markov Chain
+def generate_chain(df):
+    return df.type.value_counts() / len(df)
+
+
+# Apply smali file analysis to all smali files in an apk
+def process_apk(path):
+    df = pd.DataFrame()
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
             if name.endswith('.smali'):
                 f = open(os.path.join(root, name))
-                sml += f.read() + '\n'
+                sml_df = process_smali(f.read())
+                if type(sml_df) == int:
+                    pass
+                else:
+                    df = pd.concat([df, sml_df], ignore_index=True)
                 f.close()
-                i += 1
-    df = pd.DataFrame(re.findall(r'invoke-(\w{5,9})\s.+}, (.*);->', sml))
-    nums = df[0].value_counts()
-    def get_num(name):
-        try:
-            return nums[name]
-        except:
-            return 0
-
-    res = [ware_type, is_malware, i, df.shape[0], len(df[1].unique()), len(re.findall(r'\.method', sml)), get_num('direct'),
-           get_num('static'), get_num('virtual'), get_num('interface'), get_num('super')]
-    return res, df[1].value_counts()
+    return df
 
 
 # Parse all malware data
@@ -35,27 +55,15 @@ def parse_mal(path):
     for mal_type in ware_types:
         type_path = path + '/' + mal_type
         for variety in os.listdir(type_path):
-            res.append(generate_feature(type_path, mal_type, 1))
+            res = pd.concat([res, process_apk(type_path)], ignore_index=True)
     return res
+
 
 # Parse all benignware data
 def parse_common(path):
-    res = []
+    res = pd.DataFrame()
     wares = os.listdir(path)
-    max_w = 0
     for d in wares:
         d_path = path + '/' + d
-        res.append(generate_feature(d_path, d, 0))
-        max_w += 1
-    return re
-
-# Trasnform features list to df
-def features_todf(features):
-    return pd.DataFrame(list(pd.DataFrame(features)[0]), columns=['app_type', 'is_malware', 'num_smali','num_api', 'num_unique_api','num_block',
-       'num_direct', 'num_static', 'num_virtual', 'num_interface', 'num_super'])
-
-# Generate X, y
-def generate_Xy(df):
-    X = df.drop('is_malware', axis=1).drop('app_type', axis=1)
-    y = df.is_malware
-    return X, y
+        res = pd.concat([res, process_apk(d_path)], ignore_index=True)
+    return res
